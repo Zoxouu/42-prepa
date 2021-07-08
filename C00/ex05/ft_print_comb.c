7caaa3ca05ad240369ef69ff9c5837df5b129dff
root <root@Computer-Tibo.localdomain>
2021-07-09 00:07:36 +0200

    ajout de l'ex05 (sa commence a tere dure)

---
+++ C00/ex05/ft_print_comb.c
@@ -0,0 +1,40 @@
+#include <unistd.h>
+
+void ft_print(int a, int b, int c)
+{
+    write(1, &a, 1);
+    write(1, &b, 1);
+    write(1, &c, 1);
+}
+
+void ft_print_comb(void)
+{
+    int     a;
+    int     b;
+    int     c;
+
+    a = '0';
+    while (a <= '7')
+    {
+        b = a + 1;
+        while (b <= '8')
+        {
+            c = b + 1;
+            while (c <= '9')
+            {
+                ft_print(a, b, c);
+                if (a < '7')
+                    write(1, ", ", 2);
+                c++;
+            }
+            b++;
+        }
+        a++;
+    }
+             
+}
+
+int main(void)
+{
+    ft_print_comb();
+}
