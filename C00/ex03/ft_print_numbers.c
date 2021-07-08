6129270c8bf4066cadec3662770e64455541e2be
root <root@Computer-Tibo.localdomain>
2021-07-08 18:47:38 +0200

    ajout de l'ex03

---
+++ C00/ex03/ft_print_numbers.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+void ft_print_numbers(void)
+{
+    int i;
+
+    i = 48;// 48 = 0
+    while (i <= 57)// 57 = 9
+    {
+        write(1, &i, 1);
+        i++;
+    }
+       
+}
+
+int main(void)
+{
+    ft_print_numbers();
+    write(1, "\n", 1);
+}
